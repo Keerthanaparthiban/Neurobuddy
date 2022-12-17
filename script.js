6c35c553428482c4a8531c2b48073678cc51298c
Keerthanaparthiban <92458543+Keerthanaparthiban@users.noreply.github.com>
2022-12-17 18:10:27 +0530

    media queries upto 485px

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const navSlide = () => {
     const nav = document.querySelector('.nav-bar');
 
     burger.addEventListener('click', () => {
-        console.log('clicc=k')
+        console.log('click')
         nav.classList.toggle('nav-active');
         burger.classList.toggle('toggle');
     });
